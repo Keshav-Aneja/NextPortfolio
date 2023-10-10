e7f82c10d16930686219057db14dcdda86c989d6
Keshav-Aneja <keshav.aneja09@gmail.com>
2023-10-10 22:09:20 +0530

    btn Erro Fix

--- src/app/components/ButtonTech.tsx
+++ src/app/components/ButtonTech.tsx
@@ -4,33 +4,21 @@ interface BtnProp {
   text: string;
 }
 
-const colors = [
-  {
-    html: "#da7b56", // Orange
-  },
-  {
-    css: "#2980B9", // Blue
-  },
-  {
-    "tailwind css": "#06B6D4", // Cyan
-  },
-  {
-    javascript: "#F7DF1E", // Yellow
-  },
-  {
-    react: "#61DAFB", // Light blue
-  },
-  {
-    "next.js": "#ffffff", // Black
-  },
-];
+const colors: Record<string, string> = {
+  html: "#da7b56", // Orange
+  css: "#2980B9", // Blue
+  "tailwind css": "#06B6D4", // Cyan
+  javascript: "#F7DF1E", // Yellow
+  react: "#61DAFB", // Light blue
+  "next.js": "#ffffff", // Black
+};
 const ButtonTech: React.FC<BtnProp> = ({ text }) => {
   // Use a function in find to compare the 'text' property of each object
-  const colorObj = colors.find((color) => Object.keys(color)[0] === text);
+  // const colorObj = colors.find((color) => Object.keys(color)[0] === text);
 
   // Check if a matching color object was found
-  if (colorObj) {
-    const color = colorObj[text];
+  const color = colors[text];
+  if (color) {
     return (
       <button
         className={`px-4 md:px-6 py-2 rounded-full uppercase text-xs font-monsterrat border-b-2 hover:brightness-[300%] `}
